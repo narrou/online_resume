b3aec3fc7217f8d5fde17d62d4e143e5e2a4f14c
Julien Domont <56250573+narrou@users.noreply.github.com>
2022-07-12 15:06:48 +0200

    V2 with added icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,124 +49,135 @@ const Home: FC = () => {
 
   return (
     <>
-      {/*  {isLoading ? (
+      {isLoading ? (
         <Loader finishLoading={() => setIsLoading(false)} />
-      ) : ( */}
-      <div tw='h-screen'>
-        <Head>
-          <title>Julien Domont</title>
-        </Head>
-        <Header></Header>
-        <Side orientation='left'>
-          <ul tw='flex items-center justify-center flex-col m-0 p-0'>
-            <li>
-              <a href='https://github.com/narrou' tw='p-1'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  role='img'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  stroke='currentColor'
-                  strokeWidth='2'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  tw='w-5 h-5 align-middle hover:stroke-crayola transition hover:-translate-y-1'
-                >
-                  <title>GitHub</title>
-                  <path
-                    d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35
+      ) : (
+        <div tw='h-screen'>
+          <Head>
+            <title>Julien Domont</title>
+            <meta name='title' content='Julien Domont' />
+            <meta
+              name='og:description'
+              content='Julien Domont - Apprentice Software Engineer'
+            />
+            <meta
+              name='description'
+              content='Julien Domont - Apprentice Software Engineer'
+            />
+            <meta property='og:type' content='website' />
+            <meta property='og:url' content='juliendomont.com' />
+          </Head>
+          <Header></Header>
+          <Side orientation='left'>
+            <ul tw='flex items-center justify-center flex-col m-0 p-0'>
+              <li>
+                <a href='https://github.com/narrou' tw='p-1'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    role='img'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    tw='w-5 h-5 align-middle hover:stroke-crayola transition hover:-translate-y-1'
+                  >
+                    <title>GitHub</title>
+                    <path
+                      d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35
  6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38
   0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3
    6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'
-                  ></path>
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a href='https://linkedin.com/in/domont-julien/' tw='p-3'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  role='img'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  stroke='currentColor'
-                  strokeWidth='2'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  tw='w-5 h-5 align-middle fill-[none] hover:stroke-crayola transition hover:-translate-y-1'
-                >
-                  <title>LinkedIn</title>
-                  <path d='M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z'></path>
-                  <rect x='2' y='9' width='4' height='12'></rect>
-                  <circle cx='4' cy='4' r='2'></circle>
-                </svg>
-              </a>
-            </li>
-          </ul>
-        </Side>
-        <Side orientation='right'>
-          <a
-            href='mailto:julien.domont123@gmail.com'
-            tw='[writing-mode:vertical-rl] flex justify-center align-middle items-center text-center my-5 mx-auto'
-          >
-            julien.domont123@gmail.com
-          </a>
-        </Side>
-        <div>
-          <main tw='py-0 sm:px-24 lg:px-36 justify-center px-4 items-center'>
-            <Introduction></Introduction>
-            <About></About>
-            <Work></Work>
-            <Experience></Experience>
-            <Contact></Contact>
-          </main>
-          <footer tw='footer footer-center p-4 text-base-content gap-3 '>
-            <div tw='flex sm:hidden flex-row'>
-              <a href='https://github.com/narrou' tw='p-1'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  role='img'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  stroke='currentColor'
-                  strokeWidth='2'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  tw='w-5 h-5 align-middle hover:stroke-crayola transition hover:-translate-y-1'
-                >
-                  <title>GitHub</title>
-                  <path
-                    d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35
+                    ></path>
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href='https://linkedin.com/in/domont-julien/' tw='p-3'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    role='img'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    tw='w-5 h-5 align-middle fill-[none] hover:stroke-crayola transition hover:-translate-y-1'
+                  >
+                    <title>LinkedIn</title>
+                    <path d='M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z'></path>
+                    <rect x='2' y='9' width='4' height='12'></rect>
+                    <circle cx='4' cy='4' r='2'></circle>
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </Side>
+          <Side orientation='right'>
+            <a
+              href='mailto:julien.domont123@gmail.com'
+              tw='[writing-mode:vertical-rl] flex justify-center align-middle items-center text-center my-5 mx-auto'
+            >
+              julien.domont123@gmail.com
+            </a>
+          </Side>
+          <div>
+            <main tw='py-0 sm:px-24 lg:px-36 justify-center px-4 items-center'>
+              <Introduction></Introduction>
+              <About></About>
+              <Work></Work>
+              <Experience></Experience>
+              <Contact></Contact>
+            </main>
+            <footer tw='footer footer-center p-4 text-base-content gap-3 '>
+              <div tw='flex sm:hidden flex-row'>
+                <a href='https://github.com/narrou' tw='p-1'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    role='img'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    tw='w-5 h-5 align-middle hover:stroke-crayola transition hover:-translate-y-1'
+                  >
+                    <title>GitHub</title>
+                    <path
+                      d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35
  6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38
   0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3
    6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'
-                  ></path>
-                </svg>
-              </a>
-              <a href='https://linkedin.com/in/domont-julien/' tw='p-3'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  role='img'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  stroke='currentColor'
-                  strokeWidth='2'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  tw='w-5 h-5 align-middle fill-[none] hover:stroke-crayola transition hover:-translate-y-1'
-                >
-                  <title>LinkedIn</title>
-                  <path d='M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z'></path>
-                  <rect x='2' y='9' width='4' height='12'></rect>
-                  <circle cx='4' cy='4' r='2'></circle>
-                </svg>
-              </a>
-            </div>
-            <div>Designed & Built by Julien Domont</div>
-          </footer>
+                    ></path>
+                  </svg>
+                </a>
+                <a href='https://linkedin.com/in/domont-julien/' tw='p-3'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    role='img'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    tw='w-5 h-5 align-middle fill-[none] hover:stroke-crayola transition hover:-translate-y-1'
+                  >
+                    <title>LinkedIn</title>
+                    <path d='M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z'></path>
+                    <rect x='2' y='9' width='4' height='12'></rect>
+                    <circle cx='4' cy='4' r='2'></circle>
+                  </svg>
+                </a>
+              </div>
+              <div>Designed & Built by Julien Domont</div>
+            </footer>
+          </div>
         </div>
-      </div>
-      {/*    )} */}
+      )}
     </>
   );
 };
