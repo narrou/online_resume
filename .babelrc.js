ce4aa418c812ec90e19ea4963c8610e0476ad03e
Julien Domont <56250573+narrou@users.noreply.github.com>
2022-07-13 11:39:50 +0200

    Add files via upload

---
+++ .babelrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    [
+      'next/babel',
+      {
+        'preset-react': {
+          runtime: 'automatic',
+          importSource: '@emotion/react'
+        }
+      }
+    ]
+  ],
+  plugins: [
+    '@emotion/babel-plugin',
+    'babel-plugin-macros',
+    [
+      'babel-plugin-styled-components',
+      { ssr: true, displayName: true, preprocess: false }
+    ]
+  ]
+};
